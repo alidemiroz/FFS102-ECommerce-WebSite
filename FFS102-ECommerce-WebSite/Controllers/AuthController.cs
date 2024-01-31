36670faee986df732fc95cc2f47ee372356f3138
aliDemiroz <dmrzmehmetali@gmail.com>
2024-01-31 15:38:33 +0300

    All necessary controllers have been created.

---
+++ FFS102-ECommerce-WebSite/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FFS102_ECommerce_WebSite.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
