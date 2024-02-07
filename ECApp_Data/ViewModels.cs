e7df4db4f39222140d7d1ddccc8cac20f599c854
aliDemiroz <dmrzmehmetali@gmail.com>
2024-02-07 22:27:21 +0300

    ViewModels class have been created for able to use all models in views.

---
+++ ECApp_Data/ViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ECApp_Data
+{
+	public class ViewModels
+	{
+		public CartItem CartItem { get; set; }
+        public Category Category { get; set; }
+        public Order Order { get; set; }
+        public OrderItem OrderItem { get; set; }
+        public Product Product { get; set; }
+        public ProductComment ProductComment { get; set; }
+        public ProductImage ProductImage { get; set; }
+        public Role Role { get; set; }
+        public User User { get; set; }
+    }
+}
+
